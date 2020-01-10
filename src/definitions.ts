34083cebae3eaf91603e8a0ef1aaefa80f365d42
dariofacundodasilva <dariofacundodasilva@gmail.com>
2020-01-10 09:46:59 -0300

    definitions

--- src/definitions.ts
+++ src/definitions.ts
@@ -5,5 +5,35 @@ declare module "@capacitor/core" {
 }
 
 export interface CapacitorFirebasexPlugin {
+
+  initializeSecondaryAppFirebase(options: { 
+    apiKey: string,
+    authDomain:string,
+    databaseURL:string,
+    projectId:string,
+    storageBucket:string,
+    messagingSenderId:string,
+    appId:string,
+    measurementId:string
+  }): Promise<{}>;
+
+  initRemoteConfig(options: { minimumFetchInterval: number }): Promise<{}>;
+  initSecondaryRemoteConfig(options: { projectId: string, minimumFetchInterval: number }): Promise<{}>;
+  fetch(options: { secondary: boolean, minimumFetchInterval: number }): Promise<{}>;
+  activate(options: { secondary: boolean, minimumFetchInterval: number }): Promise<{}>;
+  fetchAndActivate(options: { secondary: boolean, minimumFetchInterval: number }): Promise<{}>;
+  getString(options: { secondary: boolean, key: string }): Promise<{value:string}>;
+  getNumber(options: { secondary: boolean, key: string }): Promise<{value:number}>;
+  getBytes(options: { secondary: boolean, key: string }): Promise<{}>;
+  getBytes(options: { secondary: boolean, key: string }): Promise<{}>;
+  getBoolean(options: { secondary: boolean, key: string }): Promise<{value:boolean}>;
+
+  setUser(options: { id: string, name: string, email:string }): Promise<{}>;
+  setCrashlyticsUserId(options: { id: string }): Promise<{}>;
+  setCrashlyticsCollectionEnabled(options:{}): Promise<{}>;
+  sendCrash(options:{}): Promise<{}>;
+  logException(options:{message:string, stacktrace:object}): Promise<{}>;
+  log(options:{message:string}): Promise<{}>;
+
   echo(options: { value: string }): Promise<{value: string}>;
 }
--- src/web.ts
+++ src/web.ts
@@ -9,6 +9,69 @@ export class CapacitorFirebasexWeb extends WebPlugin implements CapacitorFirebas
     });
   }
 
+  async initializeSecondaryAppFirebase(options: { 
+    apiKey: string,
+    authDomain:string,
+    databaseURL:string,
+    projectId:string,
+    storageBucket:string,
+    messagingSenderId:string,
+    appId:string,
+    measurementId:string
+  }): Promise<{}>{
+    return options;
+  }
+
+  async initRemoteConfig(options: { minimumFetchInterval: number }): Promise<{}>{
+    return options;
+  }
+  
+  async initSecondaryRemoteConfig(options: { projectId: string, minimumFetchInterval: number }): Promise<{}>{
+    return options;
+  }
+  async fetch(options: { secondary: boolean, minimumFetchInterval: number }): Promise<{}>{
+    return options;
+  }
+  async activate(options: { secondary: boolean, minimumFetchInterval: number }): Promise<{}>{
+    return options;
+  }
+  async fetchAndActivate(options: { secondary: boolean, minimumFetchInterval: number }): Promise<{}>{
+    return options;
+  }
+  async getString(options: { secondary: boolean, key: string }): Promise<{value:string}>{
+    return null;
+  }
+  async getNumber(options: { secondary: boolean, key: string }): Promise<{value:number}>{
+    return null;
+  }
+  async getBytes(options: { secondary: boolean, key: string }): Promise<{}>{
+    return null;
+  }
+  async getBoolean(options: { secondary: boolean, key: string }): Promise<{value:boolean}>{
+    return null;
+  }
+
+  async setUser(options: { id: string, name: string, email:string }): Promise<{}>{
+    return options;
+  }
+  async setCrashlyticsUserId(options: { id: string }): Promise<{}>{
+    return options;
+  }
+  async setCrashlyticsCollectionEnabled(options:{}): Promise<{}>{
+    return options;
+  }
+  async sendCrash(options:{}): Promise<{}>{
+    return options;
+  }
+  async logException(options:{message:string, stacktrace:object}): Promise<{}>{
+    return options;
+  }
+  async log(options:{message:string}): Promise<{}>{
+    return options;
+  }
+
+  
+
   async echo(options: { value: string }): Promise<{value: string}> {
     console.log('ECHO', options);
     return options;
